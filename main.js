910dfaa42820821ce1a7c056d741a1906710d092
tony <tonetheman@gmail.com>
2022-08-26 07:32:51 -0500

    scene is kind of working no graphics though

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ import {
     SpriteClass,
     init,
     GameLoop,
-    initInput
+    initInput,
+    Sprite,
+    randInt
 } from "./kontra/kontra.mjs";
 
 
@@ -27,7 +29,6 @@ export class Player extends SpriteClass {
 }
 
 
-
 function main() {
 
     let { canvas, context } = init();
@@ -35,11 +36,66 @@ function main() {
     let player = new Player({x : canvas.width/2, 
         y : canvas.height/2});
 
+    // bg floor parts
+    let bg1 = Sprite({
+        x : 0, y : canvas.height-32,
+        color : "green",
+        width : canvas.width,
+        height : 32,
+        dx : -1
+    });
+    let bg2 = Sprite({
+        x : canvas.width, 
+        y : canvas.height-32,
+        color : "yellow",
+        width : canvas.width,
+        height : 32,
+        dx : -1
+    });
+
+    let pBg = [];
+    for (let i=0;i<10;i++) {
+        // generate 10 background blocks
+        let tmp = Sprite({
+            x : randInt(0,canvas.width),
+            y : randInt(32, canvas.height-32),
+            dx : -0.5,
+            height : randInt(0,canvas.height/2),
+            width : 32 + randInt(0,32),
+            color : "blue"     
+        });
+        pBg[i] = tmp;
+    }
+
     function gameRender() {
+    
+        for(let i=0;i<10;i++) {
+            pBg[i].render();
+        }
+
+        bg1.render();
+        bg2.render();
+
         player.render();
+    
     }
     function gameUpdate() {
         
+        if (bg1.x<-canvas.width) bg1.x = canvas.width;
+        if (bg2.x<-canvas.width) bg2.x = canvas.width;
+
+        bg1.update();
+        bg2.update();
+
+        for (let i=0;i<10;i++) {
+
+            if (pBg[i].x+pBg[i].width<0) {
+                pBg[i].x += canvas.width+pBg[i].width;
+            }
+
+            pBg[i].update();
+        }
+
         if (player.y - player.radius > canvas.height) {
             player.dy *= -1;
         }
