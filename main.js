0842d4c256de660c940e5585780ade010ee7c6b9
tony <tonetheman@gmail.com>
2022-08-26 07:07:44 -0500

    ball is moving at least

--- main.js
+++ main.js
@@ -11,37 +11,51 @@ export class Player extends SpriteClass {
     constructor(props) {
         super({
             ...props,
-            dy : 10,
+            dy : 5,
             anchor : { x : 0.5, y : 0.5 },
             radius : 16
         });
     }
 
     draw() {
-        let { ctx, radius } = this;
-        ctx.fillStyle = '#fff';
-        ctx.beginPath();
-        ctx.arc(0,0,radius,0,2*Math.PI);
-        ctx.fill();
+        let { context, radius } = this;
+        context.fillStyle = '#f00';
+        context.beginPath();
+        context.arc(0,0,radius,0,2*Math.PI);
+        context.fill();
     }
 }
 
 
-function gameRender() {
-
-}
-function gameUpdate() {
-
-}
 
 function main() {
+
     let { canvas, context } = init();
     initInput();
+    let player = new Player({x : canvas.width/2, 
+        y : canvas.height/2});
+
+    function gameRender() {
+        player.render();
+    }
+    function gameUpdate() {
+        
+        if (player.y - player.radius > canvas.height) {
+            player.dy *= -1;
+        }
+        if (player.y-player.radius < 0) {
+            player.dy *= -1;
+        }
+
+        player.update();
+    }
+    
 
     let loop = GameLoop({
         update : gameUpdate,
         render : gameRender
     });
+    loop.start();
 }
 
 window.onload=main;
