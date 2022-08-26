df7f2bc3f63caa5e10a245528ff194c326ccdb47
tonethemna <tonetheman@gmail.com>
2022-08-26 06:51:13 -0500

    trying again with kontra

--- main.js
+++ main.js
@@ -1,87 +1,47 @@
 
-import kontra from "./kontra/kontra";
+import {
+    SpriteClass,
+    init,
+    GameLoop,
+    initInput
+} from "./kontra/kontra.mjs";
 
 
-function other_main() {
-    let canvas = kontra.getCanvas();
+export class Player extends SpriteClass {
+    constructor(props) {
+        super({
+            ...props,
+            dy : 10,
+            anchor : { x : 0.5, y : 0.5 },
+            radius : 16
+        });
+    }
 
-    let bg1 = kontra.Sprite({
-        x :0, y : 0,
-        height : 600,
-        width : 64,
-        image : kontra.imageAssets["background.png"],
-        dx : 1
-    });
-    let bg2 = kontra.Sprite({
-        x :-800, y : 0,
-        height : 600,
-        width : 64,
-        image : kontra.imageAssets["background.png"],
-        dx : 1
-    })
+    draw() {
+        let { ctx, radius } = this;
+        ctx.fillStyle = '#fff';
+        ctx.beginPath();
+        ctx.arc(0,0,radius,0,2*Math.PI);
+        ctx.fill();
+    }
+}
 
-    let sprite = kontra.Sprite({
-        x:100, y:80,
-        //color: "red",
-        image : kontra.imageAssets["whitedot_sprite_test1.png"],
-        width : 20, height : 20,
-        dy : 1
-    });
 
-    kontra.onPointer("down", function(e,o) {
-        //sprite.y -= 2;
-    });
-    kontra.onPointer("up", function(e,o){
+function gameRender() {
 
-    });
-
-    let loop = kontra.GameLoop({
-        update : function() {
-            bg1.update();
-            bg2.update();
-
-            if (bg1.x > 1600) bg1.x = 0;
-            if (bg2.x > 800) bg2.x = -800;
-
-            if (kontra.keyPressed("space")) {
-                sprite.y -= 3;
-            }
-            sprite.update();
-
-            if (sprite.x > canvas.width) {
-                sprite.x = 0;
-            }
-
-            if (sprite.y > canvas.height) {
-                sprite.y = -10;
-            }
-
-        },
-        render : function() {
-            bg1.render();
-            bg2.render();
-
-            sprite.render();
-        }
-    });
-    loop.start();
+}
+function gameUpdate() {
 
 }
 
 function main() {
+    let { canvas, context } = init();
+    initInput();
 
-        
-    kontra.init();
-    kontra.initInput();
-    kontra.initPointer();
-    kontra.initKeys();
-
-    kontra.load("whitedot_sprite_test1.png",
-        "background.png")
-    .then(function(){
-        other_main();
+    let loop = GameLoop({
+        update : gameUpdate,
+        render : gameRender
     });
-
 }
 
-window.onload = main;
+window.onload=main;
